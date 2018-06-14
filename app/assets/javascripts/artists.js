72f44c6050c5f1f42f0c7afbd25bae5a00ee93e7
bretburau <github email address>
2018-06-14 05:42:45 -0500

    Adding artists.js javascript

---
+++ app/assets/javascripts/artists.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $('#artists-link').on('click', loadArtists)
+});
+
+// let newContent; //Why is this here? TODO
+
+function loadArtists(e){
+  e.preventDefault()
+  $.get('/artists', displayArtists)
+}
+
+function displayArtists(artists) {
+  newContent = `<h2>Artists:</h2><ul>`
+  Artists.forEach((artist) => {
+    newContent += `<li><a class='btn btn-secondary' data-id='${artist.id}' href='#'>${artist.name}</a></li>`
+  });
+  newContent += `</ul>`
+  $('#contentDiv').empty().append(newContent)
+  // addListenersToArtists()
+}
