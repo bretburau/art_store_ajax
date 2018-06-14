395fa2bee8243a3a95ebc7d5141e21b73739cb7e
bretburau <github email address>
2018-06-14 05:55:19 -0500

    Artists index/show ajax working

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -14,8 +14,37 @@ function displayArtists(artists) {
   newContent = `<h2>Artists:</h2><ul>`
   artists.forEach((artist) => {
     newContent += `<li><a class='btn btn-secondary' data-id='${artist.id}' href='#'>${artist.name}</a></li>`
+    // TODO profile/artists gallery links?
   });
   newContent += `</ul>`
   $('#contentDiv').empty().append(newContent)
-  // addListenersToArtists()
+  addListenersToArtists()
+}
+
+function addListenersToArtists(){
+  let allLinks = $('a[data-id]')
+  allLinks.map((linkId) => {
+    $(`a[data-id='${linkId + 1}']`).on('click', function(e) {
+      e.preventDefault()
+      loadArtist(linkId + 1)
+    })
+  })
+}
+
+function loadArtist(id) {
+  $.get(`/artists/${id}`, displayArtist)
+}
+
+function displayArtist(artist) {
+  newContent = `<h2>Pieces by artist ${artist.name}</h2>`
+  if(artist.pieces.length === 0) {
+    newContent += `<p>No pieces from this artist yet...</p>`
+  } else {
+      newContent += '<ul>'
+    artist.pieces.forEach(function(piece){
+      newContent += `<li><a class='btn btn-secondary' onclick='loadPiece(${piece.id})' href='#'>${piece.name}</a></li>`
+    })
+    newContent += '</ul>'
+  }
+  $('#contentDiv').empty().append(newContent)
 }
